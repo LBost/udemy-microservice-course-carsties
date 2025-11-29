09bae93f0efe39b3322e2d7cc353e748730f70a1
Léon Bost <lbost@inscore.nl>
2025-11-29 11:05:50 +0100

    Moved into test folder

--- AuctionService.IntegrationTests/Fixtures/SharedFixture.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace AuctionService.IntegrationTests.Fixtures
-{
-    [CollectionDefinition("CustomWebAppFactory")]
-    public class SharedFixture : ICollectionFixture<CustomWebAppFactory>
-    {
-    }
-}
